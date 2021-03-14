9514ef5220e049979724eafa4f3db2afa59f6342
Max.S.G <max.glidden@mail.utoronto.ca>
2021-03-13 22:16:35 -0600

    Implemented TSP algorithm

--- src/contest2.cpp
+++ src/contest2.cpp
@@ -3,6 +3,7 @@
 #include <robot_pose.h>
 #include <imagePipeline.h>
 #include <cmath>
+#include <algorithm>
 
 int main(int argc, char** argv) {
     // Setup ROS.
@@ -29,28 +30,54 @@ int main(int argc, char** argv) {
     // Initialize image objectand subscriber.
     ImagePipeline imagePipeline(n);
     // Execute strategy.
+
+    // Create an adjacency matrix
     double adjMatrix[boxes.coords.size() + 1][boxes.coords.size() +1];
     
     for(int i = 0; i < boxes.coords.size() + 1; ++i) {
-        for(int j = 0, j , boxes.coords.size() + 1; ++i) {
+        for(int j = 0, j , boxes.coords.size() + 1; ++j) {
             if(i == j){
                 adjMatrix[i][j] = 0;
             }
             else if(i == 0) {
-                double dx = robotPose.x - boxes.coords[j][0];
-                double dy = robotPose.y - boxes.coords[j][1];
+                double dx = robotPose.x - boxes.coords[j-1][0];
+                double dy = robotPose.y - boxes.coords[j-1][1];
                 adjMatrix[i][j] = sqrt(dx * dx + dy * dy);
             }
             else if(j == 0) {
-                double dx = robotPose.x - boxes.coords[i][0];
-                double dy = robotPose.y - boxes.coords[i][1];
+                double dx = robotPose.x - boxes.coords[i-1][0];
+                double dy = robotPose.y - boxes.coords[i-1][1];
                 adjMatrix[i][j] = sqrt(dx * dx + dy * dy);
             }
             else {
-                double dx = boxes.coords[i][0] - boxes.coords[j][0];
-                double dy = boxes.coords[i][1] - boxes.coords[j][1];
+                double dx = boxes.coords[i-1][0] - boxes.coords[j-1][0];
+                double dy = boxes.coords[i-1][1] - boxes.coords[j-1][1];
                 adjMatrix[i][j] = sqrt(dx * dx + dy * dy);
             }
+        }
+    }
+
+    int movePlan[boxes.coords.size() + 1];
+    for(int i = 0; i < boxes.coords.size() + 1; ++i) {
+        movePlan[i] = i;
+    }
+
+    double loopCost(int movePlan, double adjMatrix) {
+        double cost = 0;
+        for(int i = 1; i < movePlan.size(); ++i) {
+            cost += adjMatrix[movePlan[i]][moveplan[i-1]];
+        }
+        cost += adjMatrix[movePlan.end()][movePlan.begin()];
+        return cost;
+    }
+
+    double bestScore = loopCost(movePlan, adjMatrix);
+    int bestRoute = movePlan;
+    while(std::next_permutation(movePlan.begin() + 1, movePlan.end())) {
+        double s = loopCost(movePlan, adjMatrix);
+        if(s < bestScore) {
+            bestScore = s;
+            bestRoute = movePlan;
         }
     }
 
