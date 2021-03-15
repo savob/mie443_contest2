732e78be650c441672617f512c4b148e18b9c5a3
Savo Bajic <bajic.savo@gmail.com>
2021-03-14 23:09:14 -0400

    Added interating through the template list

--- src/imagePipeline.cpp
+++ src/imagePipeline.cpp
@@ -93,70 +93,68 @@ int ImagePipeline::getTemplateID(Boxes& boxes) {
     std::vector<KeyPoint> keypoints_object, keypoints_scene;
     Mat descriptors_object, descriptors_scene;
 
-    // File for current tag check
-    String tagFile = "/home/brobot/catkin_ws/src/mie443_contest2/boxes_database/tag_14.jpg";
-    Mat tagImage = imread(tagFile, IMREAD_GRAYSCALE);
-    //cv::imshow("Tag checked", tagImage);
-
-    // Detect markers for the tag and the scene(img)
-    detector->detectAndCompute( tagImage, noArray(), keypoints_object, descriptors_object );
-    detector->detectAndCompute( img, noArray(), keypoints_scene, descriptors_scene );
-
-    //-- Step 2: Matching descriptor vectors with a FLANN based matcher
-    // Since SURF is a floating-point descriptor NORM_L2 is used
-    Ptr<DescriptorMatcher> matcher = DescriptorMatcher::create(DescriptorMatcher::FLANNBASED);
-    std::vector< std::vector<DMatch> > knn_matches;
-    matcher->knnMatch( descriptors_object, descriptors_scene, knn_matches, 2 );
-    //-- Filter matches using the Lowe's ratio test
-    const float ratio_thresh = 0.75f;
-    std::vector<DMatch> good_matches;
-    for (size_t i = 0; i < knn_matches.size(); i++)
-    {
-        if (knn_matches[i][0].distance < ratio_thresh * knn_matches[i][1].distance)
-        {
-            good_matches.push_back(knn_matches[i][0]);
-        }
-    }
-
-    //-- Draw matches
-    Mat img_matches;
-    drawMatches(tagImage, keypoints_object, img, keypoints_scene, good_matches, img_matches, Scalar::all(-1),
-                    Scalar::all(-1), std::vector<char>(), DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS );
-    //-- Localize the object
-    std::vector<Point2f> obj;
-    std::vector<Point2f> scene;
-    for( size_t i = 0; i < good_matches.size(); i++ )
-    {
-        //-- Get the keypoints from the good matches
-        obj.push_back( keypoints_object[ good_matches[i].queryIdx ].pt );
-        scene.push_back( keypoints_scene[ good_matches[i].trainIdx ].pt );
-    }
-
-    Mat H = findHomography( obj, scene, RANSAC );
-    //-- Get the corners from the image_1 ( the object to be "detected" )
-    std::vector<Point2f> obj_corners(4);
-    obj_corners[0] = Point2f(0, 0);
-    obj_corners[1] = Point2f( (float)tagImage.cols, 0 );
-    obj_corners[2] = Point2f( (float)tagImage.cols, (float)tagImage.rows );
-    obj_corners[3] = Point2f( 0, (float)tagImage.rows );
-    std::vector<Point2f> scene_corners(4);
-    perspectiveTransform( obj_corners, scene_corners, H);
-    //-- Draw lines between the corners (the mapped object in the scene - image_2 )
-    line( img_matches, scene_corners[0] + Point2f((float)tagImage.cols, 0),
-          scene_corners[1] + Point2f((float)tagImage.cols, 0), Scalar(0, 255, 0), 4 );
-    line( img_matches, scene_corners[1] + Point2f((float)tagImage.cols, 0),
-          scene_corners[2] + Point2f((float)tagImage.cols, 0), Scalar( 0, 255, 0), 4 );
-    line( img_matches, scene_corners[2] + Point2f((float)tagImage.cols, 0),
-          scene_corners[3] + Point2f((float)tagImage.cols, 0), Scalar( 0, 255, 0), 4 );
-    line( img_matches, scene_corners[3] + Point2f((float)tagImage.cols, 0),
-          scene_corners[0] + Point2f((float)tagImage.cols, 0), Scalar( 0, 255, 0), 4 );
-
-    imshow("Good Matches & Object detection", img_matches );
-
-    // Use: boxes.templates
     
-    cv::waitKey(0); // Wait until key pressed
-  
+    for (int tagID = 0; tagID < boxes.templates.size(); tagID++) {
+        // File for current tag check
+        Mat tagImage = boxes.templates[tagID];
+        //cv::imshow("Tag checked", tagImage);
+
+        // Detect markers for the tag and the scene(img)
+        detector->detectAndCompute( tagImage, noArray(), keypoints_object, descriptors_object );
+        detector->detectAndCompute( img, noArray(), keypoints_scene, descriptors_scene );
+
+        //-- Step 2: Matching descriptor vectors with a FLANN based matcher
+        // Since SURF is a floating-point descriptor NORM_L2 is used
+        Ptr<DescriptorMatcher> matcher = DescriptorMatcher::create(DescriptorMatcher::FLANNBASED);
+        std::vector< std::vector<DMatch> > knn_matches;
+        matcher->knnMatch( descriptors_object, descriptors_scene, knn_matches, 2 );
+        //-- Filter matches using the Lowe's ratio test
+        const float ratio_thresh = 0.75f;
+        std::vector<DMatch> good_matches;
+        for (size_t i = 0; i < knn_matches.size(); i++) {
+            if (knn_matches[i][0].distance < ratio_thresh * knn_matches[i][1].distance) {
+                good_matches.push_back(knn_matches[i][0]);
+            }
+        }
+
+        //-- Draw matches
+        Mat img_matches; // Image with matches illustrated
+        drawMatches(tagImage, keypoints_object, img, keypoints_scene, good_matches, img_matches, Scalar::all(-1),
+                        Scalar::all(-1), std::vector<char>(), DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS );
+        //-- Localize the object
+        std::vector<Point2f> obj;
+        std::vector<Point2f> scene;
+        for( size_t i = 0; i < good_matches.size(); i++ ) {
+            //-- Get the keypoints from the good matches
+            obj.push_back( keypoints_object[ good_matches[i].queryIdx ].pt );
+            scene.push_back( keypoints_scene[ good_matches[i].trainIdx ].pt );
+        }
+
+        Mat H = findHomography( obj, scene, RANSAC );
+        //-- Get the corners from the image_1 ( the object to be "detected" )
+        std::vector<Point2f> obj_corners(4);
+        obj_corners[0] = Point2f(0, 0);
+        obj_corners[1] = Point2f( (float)tagImage.cols, 0 );
+        obj_corners[2] = Point2f( (float)tagImage.cols, (float)tagImage.rows );
+        obj_corners[3] = Point2f( 0, (float)tagImage.rows );
+        std::vector<Point2f> scene_corners(4);
+        perspectiveTransform( obj_corners, scene_corners, H);
+        //-- Draw lines between the corners (the mapped object in the scene - image_2 )
+        line( img_matches, scene_corners[0] + Point2f((float)tagImage.cols, 0),
+                scene_corners[1] + Point2f((float)tagImage.cols, 0), Scalar(0, 255, 0), 4 );
+        line( img_matches, scene_corners[1] + Point2f((float)tagImage.cols, 0),
+                scene_corners[2] + Point2f((float)tagImage.cols, 0), Scalar( 0, 255, 0), 4 );
+        line( img_matches, scene_corners[2] + Point2f((float)tagImage.cols, 0),
+                scene_corners[3] + Point2f((float)tagImage.cols, 0), Scalar( 0, 255, 0), 4 );
+        line( img_matches, scene_corners[3] + Point2f((float)tagImage.cols, 0),
+                scene_corners[0] + Point2f((float)tagImage.cols, 0), Scalar( 0, 255, 0), 4 );
+
+        imshow("Good Matches & Object detection", img_matches );
+
+        // Use: boxes.templates
+        
+        cv::waitKey(0); // Wait until key pressed
+    }
     return template_id;
 }
 
