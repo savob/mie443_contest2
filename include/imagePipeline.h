8db385db66e5a5de36cb1696d2f33f40371f28fe
Savo Bajic <bajic.savo@gmail.com>
2021-03-14 22:04:12 -0400

    Implement SURF tutorial code to spot stuff

--- include/imagePipeline.h
+++ include/imagePipeline.h
@@ -11,6 +11,8 @@
 #include "opencv2/highgui.hpp"
 #include <stdio.h>
 #include <iostream>
+#include "opencv2/calib3d.hpp"
+#include "opencv2/imgproc.hpp"
 
 class ImagePipeline {
     private:
--- src/imagePipeline.cpp
+++ src/imagePipeline.cpp
@@ -64,7 +64,7 @@ int ImagePipeline::getTemplateID(Boxes& boxes) {
     // interrupted by an object, none of which have pixels of that value along the top
 
     // skyVal is defined in the header, but it can be dynamically set before this is desired.
-    
+
     for (int j = 0; j < img.cols; j++) {
         for (int i = 0; i < img.rows; i++) {
             // Go column by column from top to bottom until no longer in the sky
@@ -79,13 +79,82 @@ int ImagePipeline::getTemplateID(Boxes& boxes) {
         }
     }
 
+    using namespace cv;
+    using namespace cv::xfeatures2d;
+
     // Convert image from RGB to greyscale space
     cv::cvtColor(img, img, cv::COLOR_RGBA2GRAY, 0);
 
-    
+    //cv::imshow("Processed view. Press any key to continue.", img);
+
+    //-- Step 1: Detect the keypoints using SURF Detector, compute the descriptors
+    int minHessian = 400;
+    Ptr<SURF> detector = SURF::create( minHessian );
+    std::vector<KeyPoint> keypoints_object, keypoints_scene;
+    Mat descriptors_object, descriptors_scene;
+
+    // File for current tag check
+    String tagFile = "/home/brobot/catkin_ws/src/mie443_contest2/boxes_database/tag_14.jpg";
+    Mat tagImage = imread(tagFile, IMREAD_GRAYSCALE);
+    //cv::imshow("Tag checked", tagImage);
+
+    // Detect markers for the tag and the scene(img)
+    detector->detectAndCompute( tagImage, noArray(), keypoints_object, descriptors_object );
+    detector->detectAndCompute( img, noArray(), keypoints_scene, descriptors_scene );
+
+    //-- Step 2: Matching descriptor vectors with a FLANN based matcher
+    // Since SURF is a floating-point descriptor NORM_L2 is used
+    Ptr<DescriptorMatcher> matcher = DescriptorMatcher::create(DescriptorMatcher::FLANNBASED);
+    std::vector< std::vector<DMatch> > knn_matches;
+    matcher->knnMatch( descriptors_object, descriptors_scene, knn_matches, 2 );
+    //-- Filter matches using the Lowe's ratio test
+    const float ratio_thresh = 0.75f;
+    std::vector<DMatch> good_matches;
+    for (size_t i = 0; i < knn_matches.size(); i++)
+    {
+        if (knn_matches[i][0].distance < ratio_thresh * knn_matches[i][1].distance)
+        {
+            good_matches.push_back(knn_matches[i][0]);
+        }
+    }
+
+    //-- Draw matches
+    Mat img_matches;
+    drawMatches(tagImage, keypoints_object, img, keypoints_scene, good_matches, img_matches, Scalar::all(-1),
+                    Scalar::all(-1), std::vector<char>(), DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS );
+    //-- Localize the object
+    std::vector<Point2f> obj;
+    std::vector<Point2f> scene;
+    for( size_t i = 0; i < good_matches.size(); i++ )
+    {
+        //-- Get the keypoints from the good matches
+        obj.push_back( keypoints_object[ good_matches[i].queryIdx ].pt );
+        scene.push_back( keypoints_scene[ good_matches[i].trainIdx ].pt );
+    }
+
+    Mat H = findHomography( obj, scene, RANSAC );
+    //-- Get the corners from the image_1 ( the object to be "detected" )
+    std::vector<Point2f> obj_corners(4);
+    obj_corners[0] = Point2f(0, 0);
+    obj_corners[1] = Point2f( (float)tagImage.cols, 0 );
+    obj_corners[2] = Point2f( (float)tagImage.cols, (float)tagImage.rows );
+    obj_corners[3] = Point2f( 0, (float)tagImage.rows );
+    std::vector<Point2f> scene_corners(4);
+    perspectiveTransform( obj_corners, scene_corners, H);
+    //-- Draw lines between the corners (the mapped object in the scene - image_2 )
+    line( img_matches, scene_corners[0] + Point2f((float)tagImage.cols, 0),
+          scene_corners[1] + Point2f((float)tagImage.cols, 0), Scalar(0, 255, 0), 4 );
+    line( img_matches, scene_corners[1] + Point2f((float)tagImage.cols, 0),
+          scene_corners[2] + Point2f((float)tagImage.cols, 0), Scalar( 0, 255, 0), 4 );
+    line( img_matches, scene_corners[2] + Point2f((float)tagImage.cols, 0),
+          scene_corners[3] + Point2f((float)tagImage.cols, 0), Scalar( 0, 255, 0), 4 );
+    line( img_matches, scene_corners[3] + Point2f((float)tagImage.cols, 0),
+          scene_corners[0] + Point2f((float)tagImage.cols, 0), Scalar( 0, 255, 0), 4 );
+
+    imshow("Good Matches & Object detection", img_matches );
 
     // Use: boxes.templates
-    cv::imshow("Processed view. Press any key to continue.", img);
+    
     cv::waitKey(0); // Wait until key pressed
   
     return template_id;
@@ -96,6 +165,6 @@ void ImagePipeline::loadImage(char* fileLocation) {
     img = cv::imread(fileLocation, 1);
     isValid = true;
 
-    cv::imshow("Loaded image", img);
+    //cv::imshow("Loaded image", img);
     cv::waitKey(10);
 }
