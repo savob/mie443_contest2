3afd6add9c25721bb75791c0696f9d3beb6cc2cd
Savo Bajic <bajic.savo@gmail.com>
2021-03-20 19:14:00 -0400

    Implemented a check for if the output is successfully opened

--- src/file_write.cpp
+++ src/file_write.cpp
@@ -6,41 +6,53 @@ void writeLog(Boxes boxList, std::vector<int> movePlan, std::vector<int> boxIDs)
     std::ofstream outputFile;
     outputFile.open(logfile, std::ios_base::out | std::ios_base::trunc);
 
-    std::string headerText = "Tag ID (0 for blank/failed to ID) - Duplicate Status - Coordinates (x, y, yaw)\n";
-    outputFile << headerText;
-    
-    // ================================================
-    // Read box IDs and their coordinates into the file
+    if (outputFile.is_open()) {
 
-    // Records if a template has already appeared (plus one for blank)
-    bool alreadyTagged[boxList.templates.size() + 1];
-    for (int i = 0; i < (boxList.templates.size() + 1); i++) alreadyTagged[i] = false;
-
-    for (int i = 0; i < boxIDs.size(); i++) {
-
-        // Record tag ID for that stop
-        char tagText[10];
-        sprintf(tagText,"Tag %2d - ", boxIDs[i]);
-        outputFile << tagText;
-
-        // Record duplicate status
-        if (alreadyTagged[boxIDs[i]] == true) {
-            outputFile << "dup - ";
-        }
-        else {
-            outputFile << "new - ";
-            alreadyTagged[boxIDs[i]] = true; // Mark down it has already been listed
-        }
-
-        // Record coordinates for the stop from move list
-        std::vector<float> curCoords = boxList.coords[movePlan[i]];
+        std::string headerText = "Tag ID (0 for blank/failed to ID) - Duplicate Status - Coordinates (x, y, yaw)\n";
+        outputFile << headerText;
         
-        char coordText[25];
-        sprintf(coordText, "(%5.2f, %5.2f, %6.3f)", curCoords[0], curCoords[1], curCoords[2]);
-        outputFile << coordText;
+        // ================================================
+        // Read box IDs and their coordinates into the file
 
-        outputFile << "\n"; // Move to next line
+        // Records if a template has already appeared (plus one for blank (at index 0))
+        bool alreadyTagged[boxList.templates.size() + 1];
+        for (int i = 0; i < (boxList.templates.size() + 1); i++) alreadyTagged[i] = false;
+
+        // Go through each entry and record things
+        for (int i = 0; i < boxIDs.size(); i++) {
+
+            // Record tag ID for that stop
+            char tagText[10];
+            sprintf(tagText,"Tag %2d - ", boxIDs[i]);
+            outputFile << tagText;
+
+            // Record duplicate status
+            if (alreadyTagged[boxIDs[i]] == true) {
+                outputFile << "dup - ";
+            }
+            else {
+                outputFile << "new - ";
+                alreadyTagged[boxIDs[i]] = true; // Mark down it has already been listed
+            }
+
+            // Record coordinates for the stop from move list
+            std::vector<float> curCoords = boxList.coords[movePlan[i]];
+            
+            char coordText[25];
+            sprintf(coordText, "(%5.2f, %5.2f, %6.3f)", curCoords[0], curCoords[1], curCoords[2]);
+            outputFile << coordText;
+
+            outputFile << "\n"; // Move to next line
+
+            std::cout << tagText << coordText << "\n";
+        }
+
+        outputFile.close(); // Must close file once complete
+
+        ROS_INFO("\n\nFile written to \"%s\".\n", logfile);
     }
-
-    outputFile.close(); // Must close file once complete
+    else {
+        // File failed to open
+        ROS_FATAL("\n\nUnable to open output file.\n\n");
+    }
 }
