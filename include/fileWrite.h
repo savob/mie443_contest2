f3ef667e68891eeb9969839e9afe2dcf891f00c8
Savo Bajic <bajic.savo@gmail.com>
2021-03-21 00:36:06 -0400

    Added comment for write function

--- include/fileWrite.h
+++ include/fileWrite.h
@@ -6,4 +6,5 @@
 
 const std::string logfile = "/Documents/team22results.txt"; // Where to record (relative to home)
 
+// Function to write results to a file
 void writeLog(Boxes boxList, std::vector<int> movePlan, std::vector<int> boxIDs, bool printInfo = false);
