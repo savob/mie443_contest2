8201c98a092eda3e35bd07d028b050cab8d5c726
Savo Bajic <bajic.savo@gmail.com>
2021-03-21 00:32:06 -0400

    Changed results output file

--- include/fileWrite.h
+++ include/fileWrite.h
@@ -4,6 +4,6 @@
 #include "boxes.h"
 #include <vector>
 
-const std::string logfile = "/Documents/locations.txt"; // Where to record (relative to home)
+const std::string logfile = "/Documents/team22results.txt"; // Where to record (relative to home)
 
 void writeLog(Boxes boxList, std::vector<int> movePlan, std::vector<int> boxIDs, bool printInfo = false);
